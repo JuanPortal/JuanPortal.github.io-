735eb74099609dcd33a5ea59388abc779bcec207
Portal <juanportalg@gmail.com>
2023-05-26 16:42:24 -0700

    video correctly imported

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import video from '../assets/web_video.mp4'
 
 export const Video = () => {
   return (
     <div className='video-container'>
         <video autoPlay loop muted>
-            <source src="/assets/web_video.mp4"></source>
+            <source src={video}></source>
         </video>
     </div>
   )
