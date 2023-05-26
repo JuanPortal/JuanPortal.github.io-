06ba6ed0f4c20308825c7e3b5e93321905c9a9f6
Portal <juanportalg@gmail.com>
2023-05-26 03:47:50 -0700

    now using React

---
+++ src/TrickyQuestion.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from './Header'
+import { Form } from './Form'
+import { Video } from './Video'
+
+export const TrickyQuestion = () => {
+  return (
+    <>
+        <Header />
+        <Form />
+        <Video />
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { TrickyQuestion } from './TrickyQuestion.jsx'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TrickyQuestion />
+  </React.StrictMode>,
+)
