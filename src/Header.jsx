1ba3190f90d48f07d0edb6688db91e188750ae01
Portal <juanportalg@gmail.com>
2023-05-26 03:57:02 -0700

    structure done

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <h1>Tricky Question</h1>
+        <h2>Make your friends hate you!</h2>
+    </header>
   )
 }
--- src/TrickyQuestion.jsx
+++ src/TrickyQuestion.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Header } from './Header'
 import { Form } from './Form'
 import { Video } from './Video'
+import { Footer } from './Footer'
 
 export const TrickyQuestion = () => {
   return (
     <>
         <Header />
-        <Form />
-        <Video />
+        <section>
+            <Form />
+            <Video />
+        </section>
+        <Footer />
     </>
   )
 }
