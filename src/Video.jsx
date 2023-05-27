24b6468167e16e317e3715393f302fb84d6d5719
Portal <juanportalg@gmail.com>
2023-05-26 18:33:40 -0700

    video is now in Firebase

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import video from '../assets/web_video.mp4'
 
 export const Video = () => {
   return (
     <div className='video-container'>
         <video autoPlay loop muted title='Video of the preview of the page'>
-            <source src={video}></source>
+            <source src="https://firebasestorage.googleapis.com/v0/b/tricky-question.appspot.com/o/web_video.mp4?alt=media&token=8c3bc2fb-0304-44a0-937f-e64615c7427d"></source>
         </video>
     </div>
   )
