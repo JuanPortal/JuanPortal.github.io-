9ab9388f19e36f0d34b9530e4dc8e39114b64edd
Portal <juanportalg@gmail.com>
2023-05-26 18:40:38 -0700

    footer updated

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer>
-        Footer
+        Developed by <a href="https://juanportalg.com" target='_blank'>Juan Portal</a>
     </footer>
   )
 }
