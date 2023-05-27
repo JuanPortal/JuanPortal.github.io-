0da8f8bf10f8db8a868048175840836593935f92
Portal <juanportalg@gmail.com>
2023-05-26 17:03:10 -0700

    yes image implemented

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export const Question = () => {
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const question = params.get('question');
-  const yes = params.get('yes');
-  const no = params.get('no');
-  
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const question = params.get('question');
+    const yes = params.get('yes');
+    const no = params.get('no');
 
     const movingButton = ({ target }) => {
         target.style.top = `${Math.floor(Math.random() * 80)}%`
@@ -15,12 +14,24 @@ export const Question = () => {
         console.log(`top: ${target.style.top}    left: ${target.style.left}`)
     }
 
+    const [funAreaVisibility, setFunAreaVisibility] = useState('')
+    const [yesAreaVisibility, setYesAreaVisibility] = useState('hidden')
+
+    const onYesClick = () => {
+        setFunAreaVisibility('hidden')
+        setYesAreaVisibility('')
+    }
+
     return (
         <section className='question'>
             <h3>{question}</h3>
-            <div className="fun-area">
-                <div className='button'>{yes || "Yes"}</div>
+            <div className={`fun-area ${funAreaVisibility}`}>
+                <div onClick={onYesClick} className='button'>{yes || "Yes"}</div>
                 <div onClick={movingButton} onMouseOver={movingButton} className='button'>{no || "No"}</div>
+            </div>
+
+            <div className={`yes-area ${yesAreaVisibility}`}>
+                <img src="https://firebasestorage.googleapis.com/v0/b/tricky-question.appspot.com/o/yes.webp?alt=media&token=fbf1d76d-6921-4cf1-838c-442455d7c8c3" alt="suprised monkey meme" title='suprised monkey meme'/>
             </div>
         </section>
     )
--- src/Video.jsx
+++ src/Video.jsx
@@ -4,7 +4,7 @@ import video from '../assets/web_video.mp4'
 export const Video = () => {
   return (
     <div className='video-container'>
-        <video autoPlay loop muted>
+        <video autoPlay loop muted title='Video of the preview of the page'>
             <source src={video}></source>
         </video>
     </div>
